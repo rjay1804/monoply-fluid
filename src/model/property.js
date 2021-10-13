1c192e3270d86bbbf08303b2084ff54916d9e19f
Rahul Jain <rahujain@microsoft.com>
2021-10-13 14:12:37 +0530

    property.js

--- src/model/property.js
+++ src/model/property.js
@@ -1,13 +1,17 @@
 class Property {
-    constructor(rent, price, type) {
-        this.rent_ = rent;
-        this.price = price;
-        this.type_ = type;
-    }
 
-    rent_ = 0;
-    price_ = 0;
-    type_;
+    rent = 100;
+    price = 1000;
+
+    // constructor(rent, price, type) {
+    //     this.rent_ = rent;
+    //     this.price = price;
+    //     this.type_ = type;
+    // }
+
+    // rent_ = 0;
+    // price_ = 0;
+    // type_;
 }
 
 module.export = Property;
