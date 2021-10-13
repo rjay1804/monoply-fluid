6b336b486be44f7255726a5ceb5a3faba6e0d7c4
Ashish Kumar <ashiskumar@microsoft.com>
2021-10-13 13:55:23 +0530

    adding cell class

---
+++ src/model/cell.js
@@ -0,0 +1,32 @@
+class cell {
+    constructor(row, col) {
+        this.row_ = row;
+        this.col_ = col;
+    }
+
+    enter(player) {
+
+    }
+    exit(player) {
+
+    }
+    setColor(color) {
+        color_ = color;
+    }
+    setId(id) {
+        id_ = id;
+    }
+    rowId() {
+        return this.row_;
+    }
+    colId() {
+        return this.col_;
+    }
+
+    id_ = 0;
+    color_ = "white"
+    price_ = 0;
+    owner_ = "bank";
+    row_ = -1;
+    col_ = -1;
+}
