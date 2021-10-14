43297d79056afb493daeca520fc9d4cbd58e3401
Harivallabha Rangarajan <hrangarajan@microsoft.com>
2021-10-14 12:54:58 +0530

    Add entire folder post deletion of unncessary files

--- src/helpers/puzzles.ts
+++ src/helpers/puzzles.ts
@@ -8,64 +8,9 @@ import { ISharedMap } from "@fluidframework/map";
 import { Coordinate } from "./coordinate";
 import { SudokuCell } from "./sudokuCell";
 
-import {wordfind} from '../../public/wordfind'
-//import {cli_browser} from '../../public/cli-browser'
-//import {monopoly} from '../../public/monopoly';
-//import {wordfindgame} from '../../public/wordfindgame'
+import {wordfind} from '../../public/wordfind';
 
 //declare var WordFindGame: any;
-
-const { objFillDefaults } = require('../../public/helper');
-const { EventEmitter } = require('events');
-
-const Team = require('../../public/Team');
-class Monopoly extends EventEmitter {
-
-    constructor(options = {}) {
-      super();
-  
-      //const self = this;
-      console.log("Random check");
-      const opts = objFillDefaults(options, {
-        board: {},
-        teams: 2,
-        wallet: 5000
-      });
-  
-  
-      opts.board.pieces = opts.teams;
-      this.board = new Monopoly.MonopolyBoard(opts.board);
-  
-      this.teams = new Array(opts.teams).fill(true).map((value, index) => {
-        return new Team(this, index, opts.wallet);
-      });
-  
-      this.currentAction = null;
-  
-  
-    }
-    payment(payingTeam, targetTeam, amount) {
-      payingTeam.wallet -= amount;
-      targetTeam.wallet += amount;
-  
-      return {};
-    }
-    action(actionRecord, team) {
-      return this.board.actions.methods[actionRecord.type](team, actionRecord.fields);
-    }
-    // Firing all related eventsto next actions
-    nextActions(actions) {
-      for (let actionName in actions) {
-        if (actions.hasOwnProperty(actionName)) {
-          // If current action is valid
-          if (actions[actionName]) {
-            // Emit the action related event from its name with the action specific data
-            this.emit("payment", actions[actionName]);
-          }
-        }
-      }
-    }
-  }
 
 
 /**
@@ -169,21 +114,6 @@ export function loadPuzzle(index: number, puzzleMap: ISharedMap, solutionMap: IS
 
     console.log(solution.found);
     
-
-
-    Monopoly.MonopolyBoard = require('../monopoly-board');
-
-    // if (typeof window !== 'undefined') {
-    //   window.Monopoly = Monopoly;
-    // }
-
-    console.log("Check 180");
-    
-    
-    //module.exports = Monopoly;
-    console.log("Try to create Monopoly board");
-    var newGame = new Monopoly;
-    console.log(newGame);
 
     var i:number; 
 
