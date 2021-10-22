18a11fc8cfd7854df8dc68f3e31286e34c819598
Harivallabha Rangarajan <hrangarajan@microsoft.com>
2021-10-22 11:15:41 +0530

    restore fluid gameboard

--- src/view/client/GameBoard.tsx
+++ src/view/client/GameBoard.tsx
@@ -1,15 +1,132 @@
-import React from "react";
 import { GameSquare } from "./GameSquare";
-//import { IMonopolyViewProps } from "../../react/monopolyView";
 import { get_props } from "../../react/monopolyView";
-//import { playerName } from "../../helpers/puzzles";
-//import { PropertyDisplay } from "./squares/PropertyDisplay";
-//import  {props.clientPlayerMap} from "../../react/monopolyView";
+import {Dice} from "../../model/Dice";
+
+import React from "react";
+import styled from "styled-components";
+//mport { Avatar } from "@material-ui/core";
+//import { Avatar } from 'react-native-paper'; 
+//const MyComponent = () => ( <Avatar src={require('../../images/Die_4.png')} /> );
+
+var die = new Dice(6);
+var set_die_image = 0;
+var props_g;
+var diceChar;
+var size_g;
+// var set_die_image_path = "../../images/Die_1.png";;
+
+// const image_1 = require('../../images/Die_1.png');
+// const image_2 = require('../../images/Die_2.png');
+// const image_3 = require('../../images/Die_3.png');
+// const image_4 = require('../../images/Die_4.png');
+// const image_5 = require('../../images/Die_5.png');
+// const image_6 = require('../../images/Die_6.png');
+
+//var img_load;
+
+const theme = {
+  blue: {
+    default: "#3f51b5",
+    hover: "#283593"
+  },
+  pink: {
+    default: "#e91e63",
+    hover: "#ad1457"
+  }
+};
+
+
+
+
+const Button = styled.button`
+  background-color: ${(props) => theme[props.theme].default};
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: ${(props) => theme[props.theme].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+Button.defaultProps = {
+  theme: "blue"
+};
+
+function clickMe() {
+  alert("Rolling...");
+  set_die_image = die.rollDice();
+  console.log("Die roll:");
+  console.log(set_die_image);
+  alert(set_die_image);
+  console.log("Intial: ", props_g.whoseTurn.get("whoseturn"));
+  var current_turn = props_g.whoseTurn.get("whoseturn");
+  var next = (current_turn + 1) % (size_g + 1)
+  if(next == 0)
+  {
+    next+=1;
+  }
+  props_g.whoseTurn.set("whoseturn", next); 
+  console.log("Number of players:", size_g);
+  console.log("After move:", props_g.whoseTurn.get("whoseturn"));
+  diceChar = String.fromCodePoint(0x267F+ set_die_image);
+  //props, currentplayer
+  //Get new square
+  //Pass new square to change loc
+
+  //set_image(set_die_image);
+  //set_die_image_path = "../../images/Die_4.png";
+
+}
+
+// function set_image(idx)
+// {
+//   if (idx == 1)
+//   {
+//     return image_1;
+//   }
+//   if (idx == 2)
+//   {
+//     img_load =  image_2;
+//   }
+//   if (idx == 3)
+//   {
+//     img_load =  image_3;
+//   }
+//   if (idx == 4)
+//   {
+//     img_load = image_4;
+//   }
+//   if (idx == 5)
+//   {
+//     img_load =  image_5;
+//   }
+//   if (idx == 6)
+//   {
+//     img_load =  image_6;
+//   }
+//   console.log(img_load);
+//   console.log(idx);
+// }
+
+
 
 export default function GameBoard() {
- const num_squares: Array<number> = Array.from(Array(40));
+  const num_squares: Array<number> = Array.from(Array(40));
+  
   var props = get_props();
+  props_g = props;
   var size_ = props.playerNameMap.size;
+  size_g = size_;
   console.log("Size: ");
   console.log(size_);
   //var key_list = get_keys();
@@ -20,7 +137,7 @@ export default function GameBoard() {
   // console.log(keys_);
   // console.log(playerNames);
   var playerNames = new Array<String>();
-  console.log("Checking...");
+  console.log("img_loading...");
   var idx = 0;
   for(idx = 0; idx <size_; idx++){
     //console.log("hey");
@@ -30,10 +147,10 @@ export default function GameBoard() {
   }
   console.log(playerNames);
 
-  for(idx = 1; idx <size_; idx++){
-    //console.log("hey");
-    console.log(props.clientPlayerMap.get(playerNames[idx]).changeScore(idx));
-  }
+  // for(idx = 1; idx <size_; idx++){
+  //   //console.log("hey");
+  //   console.log(props.clientPlayerMap.get(playerNames[idx]).changeScore(idx));
+  // }
 
   //var die = 1;
   //var path_ = "C:\\Users\\hrangarajan\\Desktop\\Work\\Hackathon\\monopoly-fluid\\src\\images\\Die_1.png";
@@ -49,19 +166,17 @@ export default function GameBoard() {
             key={id}
           />)
         })}
-          
-        <div className="center-square square">
-           <div className="center-txt">  
-          <table border = "1">
+
+          <table >
           <tbody>
-            <tr border = "1">
+            <tr >
               <th> Player Name </th>
               <th> Cash Available </th>
               </tr >
               
               
               
-              <tr border = "1">
+              <tr >
             <td>
              {playerNames.map(name__ => (  
                 <tr>{name__}</tr>
@@ -78,12 +193,25 @@ export default function GameBoard() {
             </tr>
             </tbody>
           </table >
+          
+        <div className="center-square square">
+           <div className="center-txt">  
+
+              {/* <Button onClick={clickMe}>Roll</Button>
+              <span style={{fontSize: 50 }}>{diceChar}</span>
+              
+              <img src= {require('../../images/Die_6.png')}/> */}
+              
+            
+            
+
             <a href="https://fluidframework.com/docs/">Powered by Fluid</a>
            
             {/* <img src="https://imgsv.imaging.nikon.com/lineup/dslr/df/img/sample/img_02_l.jpg"> </img> */}
            </div>
         </div> 
       </div>
+      
     </React.Fragment>
     
   );
@@ -100,3 +228,5 @@ export default function GameBoard() {
 //     {name}  
 //   </li>  
 // ))}  
+
+
